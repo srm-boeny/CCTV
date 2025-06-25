05f0a1b42afd44ca5964410988233f9b7928968d
Andriamihaja R. <andriamihajaranaivoson@yahoo.fr>
2025-06-25 07:12:22 +0300

    Forecast on 25/06/2025

--- script.js
+++ script.js
@@ -3,26 +3,27 @@ const niveauAlerte = "none";       // Options: "none", "blue", "green", "yellow"
 const typeCatastrophe = "cyclone"; // Options: "cyclone", "flood", "rainflood", "forestfire", "lightning", "flood", "stormsurge", "drivingconditions"
 
 const messages = [
-    `✅ **Aucune vigilance ou alerte en cours pour la Région BOENY**  
+    `**Prévisions journalières pour la Région BOENY, établi le 25/06/2025 à 06:00 AM**
+	✅ **Aucune vigilance ou alerte en cours pour la Région BOENY**  
     Les conditions météorologiques sont calmes pour les 3 jours.  
     Restez informé en cas d’évolution de la situation.`,
 
     ` **Prévisions – Jour 1 (Aujourd’hui)**  
-    Temps ensoleillé toute la matinée et peu nuageux l'après-midi et le soir à l'Ouest de la Région.
-    Vents : Vents légers à modéré de secteur E
-    Températures minimales : avoisinant les 19 °C
-    Températures maximales : avoisinant les 33 °C.`,
+    Temps ensoleillé toute la matinée et peu nuageux l'après-midi et le soir.
+    Vents : Vents modéré à fort de secteur ESE
+    Températures minimales : entre 20 et 21 °C
+    Températures maximales : entre 30 et 33 °C.`,
 
     ` **Prévisions – Jour 2 (Demain)**  
     Temps ensoleillé le matin, devenant partiellement nuageux l'après-midi et le soir.
     Vents : Vents modéré du secteur ESE
-    Températures minimales : entre 18 et 20 °C
+    Températures minimales : entre 19 et 20 °C
     Températures maximales : entre 30 et 33 °C.`,
 
     ` **Prévisions – Jour 3 (Après-demain)**  
-    Temps ensoleillé le matin, nuageux l'après-midi et le soir.
-    Vents : Vents modéré de secteur E
-    Températures minimales : entre 19 et 20 °C
+    Temps ensoleillé le matin, partielement nuageux l'après-midi et le soir dans les parties Est de la Région.
+    Vents : Vents modéré de secteur ESE
+    Températures minimales : entre 20 et 22 °C
     Températures maximales : entre 32 et 33 °C.`
 ];
 
